2869b474b477e3164207e9d3d88f53a760f2209b
Don Shults <don.shults@neudesic.com>
2017-09-05 11:59:16 -0700

    setup aws-pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -28,9 +28,7 @@ var s3 = require('gulp-s3-upload')(config);
 //const lambda = require('gulp-lambda-deploy');
 
 gulp.task('setawsprofile', function () {
-    console.log(process.env.AWS_PROFILE);    
-    console.log(process.env.aws_access_key_id);
-    console.log(process.env.aws_secret_access_key);
+    awsIdentity()
 });
 
 jenkins.init({username:'admin', password: 'Deepsp@ce9', url: '34.211.169.164:8080'})
